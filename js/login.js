3d11a41dd073997bbdb45e60f9c6ee7e4b8e0f7e
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-02 13:10:31 +0600

    navigate login page to home page

--- js/login.js
+++ js/login.js
@@ -1,8 +1,15 @@
 
 document.getElementById('btn-login').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('click');
-    
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber);
-})
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+    if(phoneNumber.length ===  11  && pinNumber.length === 5){
+      console.log('You are logged in');
+      window.location.href = '/home.html';
+    }
+    else{
+        alert('Please provided  pin maximum 5 digit and  Number maximum 11 digit');
+    }
+});
