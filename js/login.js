409ce288f752d30ae8df1c71c924939e30f708f6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-02 12:24:33 +0600

    get login value

---
+++ js/login.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btn-login').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('click');
+    
+    const phoneNumber = document.getElementById('phone-number');
+    console.log(phoneNumber);
+})
