cb9fb8b12f1146eac0717d950f99fe3865c2412e
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-02 19:48:50 +0600

    routing add

---
+++ js/route.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-CashOut').addEventListener('click', function(e){
+    e.preventDefault();
+   document.getElementById('cashOut-form').classList.remove('hidden');
+   document.getElementById('addMoney-form').classList.add('hidden');
+});
+
+document.getElementById('btn-addMoney').addEventListener('click', function(e){
+    e.preventDefault();
+    document.getElementById('addMoney-form').classList.remove('hidden');
+    document.getElementById('cashOut-form').classList.add('hidden');
+});
